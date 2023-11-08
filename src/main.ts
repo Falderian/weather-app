76e48efb9039d38a4a143f134f10e941646025ca
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-11-08 16:21:32 +0300

    UPD: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 4444;
   const app = await NestFactory.create(WeatherModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(PORT, () =>
     console.log('Server is working on the ' + PORT + ' port'),
   );
